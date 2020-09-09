8a5040737f40cd31549ca5628507118dac516dbf
Dwarka prasad bairwa <56918814+dp1706@users.noreply.github.com>
2020-09-09 23:17:57 +0530

    Update apache_script.sh

--- assignment 4/apache_script.sh
+++ assignment 4/apache_script.sh
@@ -2,8 +2,8 @@
 yum -y update
 yum -y install httpd
 service httpd start
-aws configure set aws_access_key_id AKIAIKGFXVQQQQSR5YVQ
-aws configure set aws_secret_access_key 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
+aws configure set aws_access_key_id AKIAIKxxxxxxxxxxxxxxxxx
+aws configure set aws_secret_access_key MMbLhCzdmL9Kt6/Exxxxxxxxxxxxxxxxxxxxxx
 aws configure set default.region us-east-2
 aws s3 cp s3://dwarka/index.html /var/www/html/
 aws s3 cp s3://dwarka/style.css /var/www/html/
